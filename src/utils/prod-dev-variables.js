cbf5e1dce3a2e1a3b1996118dcd172c724834d8b
yonext24 <yonielkpo@gmail.com>
2023-10-19 19:07:44 -0300

    Changed backend url

--- src/utils/prod-dev-variables.js
+++ src/utils/prod-dev-variables.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-undef */
 export const isProduction = process.env.NODE_ENV === 'production'
 
-export const API_ADMIN_URL = 'https://optometry-back.vercel.app/api/user'
-export const API_APPOINTMENTS =
-  'https://optometry-back.vercel.app/api/appointments'
+export const API_ADMIN_URL = 'https://opto-back.vercel.app/api/user'
+export const API_APPOINTMENTS = 'https://opto-back.vercel.app/api/appointments'
